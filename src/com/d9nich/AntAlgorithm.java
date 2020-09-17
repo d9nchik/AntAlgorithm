da9d62a2b9825ecb90e0a620cf2e954c1099d4bb
d9nich <dan.galaiko@gmail.com>
2020-09-17 14:41:42 +0300

    Generator of matrix of distance has been done.

---
+++ src/com/d9nich/AntAlgorithm.java
@@ -0,0 +1,4 @@
+package com.d9nich;
+
+public class AntAlgorithm {
+}
--- src/com/d9nich/Main.java
+++ src/com/d9nich/Main.java
@@ -1,8 +1,10 @@
 package com.d9nich;
 
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Arrays.stream(MatrixDistanceGenerator.generate(15)).forEach(e -> System.out.println(Arrays.toString(e)));
     }
 }
