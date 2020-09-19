55952eacd1dd0c7d5e752cc1155bf4c227cd0cec
d9nich <dan.galaiko@gmail.com>
2020-09-19 10:05:10 +0300

    Refactoring of AntAlgorithm.

--- src/com/d9nich/Main.java
+++ src/com/d9nich/Main.java
@@ -8,7 +8,7 @@ public class Main {
         final int[][] matrixOfDistance = MatrixDistanceGenerator.generate(150);
 //        Arrays.stream(matrixOfDistance).forEach(e -> System.out.println(Arrays.toString(e)));
 
-        final int[] shortestDistance = AntAlgorithm.findShortestDistance(matrixOfDistance);
+        final int[] shortestDistance = new AntAlgorithm(matrixOfDistance).getPath();
         System.out.println("Path: ");
         for (int number : shortestDistance) {
             System.out.print(number + 1 + "->");
--- src/com/d9nich/pathFindingAlgorithm/antAlgorithm/AntAlgorithm.java
+++ src/com/d9nich/pathFindingAlgorithm/antAlgorithm/AntAlgorithm.java
@@ -5,49 +5,57 @@ import com.d9nich.pathFindingAlgorithm.PathFindable;
 
 import java.util.Random;
 
-public class AntAlgorithm {
+public class AntAlgorithm implements PathFindable {
 
     public static final double RO = 0.4;//ρ
     public static final int ANTS_NUMBER = 35;
 
-    public static int[] findShortestDistance(int[][] distanceMatrix) {
-        final double[][] PHEROMONE = new double[distanceMatrix.length][distanceMatrix.length];
+    private final int[][] DISTANCE_MATRIX;
+    private final double[][] MATRIX_OF_REVERSED_DISTANCE;
+    private final double[][] PHEROMONE;
+    private int[] path;
+    private double pathLength;
+
+    public AntAlgorithm(int[][] DISTANCE_MATRIX) {
+        this.DISTANCE_MATRIX = DISTANCE_MATRIX;
+        MATRIX_OF_REVERSED_DISTANCE = createMatrixOfReversedDistance(DISTANCE_MATRIX);
+        PHEROMONE = createMatrixOfPheromone(DISTANCE_MATRIX.length);
+        PathFindable pathFindable = new GreedyAlgorithm(DISTANCE_MATRIX);
+        pathLength = Ant.Lmin = pathFindable.getLength();
+        path = pathFindable.getPath();
+        findShortestDistance();
+    }
+
+    private static double[][] createMatrixOfPheromone(int length) {
+        final double[][] PHEROMONE = new double[length][length];
         for (int i = 0; i < PHEROMONE.length; i++) {
             for (int j = 0; j < PHEROMONE.length; j++) {
                 if (i != j)
                     PHEROMONE[i][j] = 0.1;
             }
         }
+        return PHEROMONE;
+    }
 
-        double[][] matrixOfReversedDistance = createMatrixOfReversedDistance(distanceMatrix);
-        final Ant[] ants = new Ant[ANTS_NUMBER];
-        Random random = new Random();
-        //making popularity
-        for (int i = 0; i < ants.length; i++) {
-            ants[i] = new Ant(PHEROMONE, matrixOfReversedDistance, distanceMatrix, random.nextInt(distanceMatrix.length));
-        }
+    private void findShortestDistance() {
+        final Ant[] ants = makeColony();
 
-        PathFindable pathFindable = new GreedyAlgorithm(distanceMatrix);
-        double length = Ant.Lmin = pathFindable.getLength();
-        int[] path = pathFindable.getPath();
-        System.out.println("Greedy algorithm length: " + length);
+        System.out.println("Greedy algorithm length: " + pathLength);
         for (int i = 0; i < 1_000; i++) {
             for (Ant ant : ants) {
                 final double antPathLength = ant.walkThrough();
-                if (antPathLength < length) {
-                    length = antPathLength;
+                if (antPathLength < pathLength) {
+                    pathLength = antPathLength;
                     path = ant.getPath();
                 }
             }
             updatePheromone(ants, PHEROMONE);
-            Ant.Lmin = length;
+            Ant.Lmin = pathLength;
             //Printing path length
             if (i % 20 == 0) {
-                System.out.println(i + ") " + length);
+                System.out.println(i + ") " + pathLength);
             }
         }
-
-        return path;
     }
 
     private static double[][] createMatrixOfReversedDistance(int[][] distanceMatrix) {
@@ -72,5 +80,26 @@ public class AntAlgorithm {
         for (Ant ant : ants) {
             ant.spreadPheromone();
         }
+    }
+
+    private Ant[] makeColony() {
+        final Ant[] ants = new Ant[ANTS_NUMBER];
+        Random random = new Random();
+        //making popularity
+        for (int i = 0; i < ants.length; i++) {
+            ants[i] = new Ant(PHEROMONE, MATRIX_OF_REVERSED_DISTANCE, DISTANCE_MATRIX,
+                    random.nextInt(DISTANCE_MATRIX.length));
+        }
+        return ants;
+    }
+
+    @Override
+    public int[] getPath() {
+        return path;
+    }
+
+    @Override
+    public double getLength() {
+        return pathLength;
     }
 }
